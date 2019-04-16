37a52905e64453d5f18cdacdf27a485cd9562914
Jason Keene <jkeene@pivotal.io>
2019-04-16 12:07:21 -0600

    output: add function to set context on the plugin
    This change also updates the argument names to the other helper
    functions to match more accurately what is being called with in
    fluent-bit.
    
    Signed-off-by: Warren Fernandes <wfernandes@pivotal.io>

--- output/output.go
+++ output/output.go
@@ -23,44 +23,51 @@ package output
 #include "flb_output.h"
 */
 import "C"
-import "fmt"
-import "unsafe"
+import (
+	"unsafe"
+)
 
 // Define constants matching Fluent Bit core
-const FLB_ERROR               =  C.FLB_ERROR
-const FLB_OK                  =  C.FLB_OK
-const FLB_RETRY               =  C.FLB_RETRY
+const (
+	FLB_ERROR = C.FLB_ERROR
+	FLB_OK    = C.FLB_OK
+	FLB_RETRY = C.FLB_RETRY
 
-const FLB_PROXY_OUTPUT_PLUGIN =  C.FLB_PROXY_OUTPUT_PLUGIN
-const FLB_PROXY_GOLANG        =  C.FLB_PROXY_GOLANG
+	FLB_PROXY_OUTPUT_PLUGIN = C.FLB_PROXY_OUTPUT_PLUGIN
+	FLB_PROXY_GOLANG        = C.FLB_PROXY_GOLANG
+)
 
 // Local type to define a plugin definition
-type FLBPlugin C.struct_flb_plugin_proxy
+type FLBPluginProxyDef C.struct_flb_plugin_proxy_def
 type FLBOutPlugin C.struct_flbgo_output_plugin
 
 // When the FLBPluginInit is triggered by Fluent Bit, a plugin context
 // is passed and the next step is to invoke this FLBPluginRegister() function
 // to fill the required information: type, proxy type, flags name and
 // description.
-func FLBPluginRegister(ctx unsafe.Pointer, name string, desc string) int {
-	p := (*FLBPlugin) (unsafe.Pointer(ctx))
+func FLBPluginRegister(def unsafe.Pointer, name, desc string) int {
+	p := (*FLBPluginProxyDef)(def)
 	p._type = FLB_PROXY_OUTPUT_PLUGIN
 	p.proxy = FLB_PROXY_GOLANG
 	p.flags = 0
-	p.name  = C.CString(name)
+	p.name = C.CString(name)
 	p.description = C.CString(desc)
 	return 0
 }
 
 // Release resources allocated by the plugin initialization
-func FLBPluginUnregister(ctx unsafe.Pointer) {
-	p := (*FLBPlugin) (unsafe.Pointer(ctx))
-	fmt.Printf("[flbgo] unregistering %v\n", p)
+func FLBPluginUnregister(def unsafe.Pointer) {
+	p := (*FLBPluginProxyDef)(def)
 	C.free(unsafe.Pointer(p.name))
 	C.free(unsafe.Pointer(p.description))
 }
 
-func FLBPluginConfigKey(ctx unsafe.Pointer, key string) string {
+func FLBPluginConfigKey(plugin unsafe.Pointer, key string) string {
 	_key := C.CString(key)
-	return C.GoString(C.output_get_property(_key, unsafe.Pointer(ctx)))
+	return C.GoString(C.output_get_property(_key, plugin))
+}
+
+func FLBPluginSetContext(plugin, ctx unsafe.Pointer) {
+	p := (*FLBOutPlugin)(plugin)
+	p.context.remote_context = ctx
 }
