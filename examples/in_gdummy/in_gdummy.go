03ba2b0da77cb985e61164ac36da4c2c40b777f3
Hiroshi Hatake <hatake@calyptia.com>
2022-02-25 17:43:13 +0900

    input: Implement Golang Input plugin interface and added input example
    Signed-off-by: Hiroshi Hatake <hatake@calyptia.com>

---
+++ examples/in_gdummy/in_gdummy.go
@@ -0,0 +1,56 @@
+package main
+
+import "C"
+import (
+	"fmt"
+	"time"
+	"unsafe"
+
+	"github.com/fluent/fluent-bit-go/input"
+)
+
+//export FLBPluginRegister
+func FLBPluginRegister(def unsafe.Pointer) int {
+	return input.FLBPluginRegister(def, "gdummy", "dummy GO!")
+}
+
+//export FLBPluginInit
+// (fluentbit will call this)
+// plugin (context) pointer to fluentbit context (state/ c code)
+func FLBPluginInit(plugin unsafe.Pointer) int {
+	// Example to retrieve an optional configuration parameter
+	param := input.FLBPluginConfigKey(plugin, "param")
+	fmt.Printf("[flb-go] plugin parameter = '%s'\n", param)
+	return input.FLB_OK
+}
+
+//export FLBPluginInputCallback
+func FLBPluginInputCallback(data *unsafe.Pointer, size *C.size_t) int {
+	now := time.Now()
+	flb_time := input.FLBTime{now}
+	message := map[string]string{"message": "dummy"}
+
+	entry := []interface{}{flb_time, message}
+
+	enc := input.NewEncoder()
+	packed, err := enc.Encode(entry)
+	if err != nil {
+		fmt.Println("Can't convert to msgpack:", message, err)
+		return input.FLB_ERROR
+	}
+
+	*data = unsafe.Pointer(&packed[0])
+	*size = C.size_t(len(packed))
+	// For emitting interval adjustment.
+	time.Sleep(1000 * time.Millisecond)
+
+	return input.FLB_OK
+}
+
+//export FLBPluginExit
+func FLBPluginExit() int {
+	return input.FLB_OK
+}
+
+func main() {
+}
