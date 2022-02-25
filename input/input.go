54d31a123b2ffa2d3eeb3fdf432215789d7cb896
Hiroshi Hatake <hatake@calyptia.com>
2022-02-25 17:43:13 +0900

    input: Implement Golang Input plugin interface and added input example
    Signed-off-by: Hiroshi Hatake <hatake@calyptia.com>

---
+++ input/input.go
@@ -0,0 +1,70 @@
+//  Fluent Bit Go!
+//  ==============
+//  Copyright (C) 2022 The Fluent Bit Go Authors
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+package input
+
+/*
+#include <stdlib.h>
+#include "flb_plugin.h"
+#include "flb_input.h"
+*/
+import "C"
+import (
+	"unsafe"
+)
+
+// Define constants matching Fluent Bit core
+const (
+	FLB_ERROR = C.FLB_ERROR
+	FLB_OK    = C.FLB_OK
+	FLB_RETRY = C.FLB_RETRY
+
+	FLB_PROXY_INPUT_PLUGIN = C.FLB_PROXY_INPUT_PLUGIN
+	FLB_PROXY_GOLANG       = C.FLB_PROXY_GOLANG
+)
+
+// Local type to define a plugin definition
+type FLBPluginProxyDef C.struct_flb_plugin_proxy_def
+type FLBInPlugin C.struct_flbgo_input_plugin
+
+// When the FLBPluginInit is triggered by Fluent Bit, a plugin context
+// is passed and the next step is to invoke this FLBPluginRegister() function
+// to fill the required information: type, proxy type, flags name and
+// description.
+func FLBPluginRegister(def unsafe.Pointer, name, desc string) int {
+	p := (*FLBPluginProxyDef)(def)
+	p._type = FLB_PROXY_INPUT_PLUGIN
+	p.proxy = FLB_PROXY_GOLANG
+	p.flags = 0
+	p.name = C.CString(name)
+	p.description = C.CString(desc)
+	return 0
+}
+
+// Release resources allocated by the plugin initialization
+func FLBPluginUnregister(def unsafe.Pointer) {
+	p := (*FLBPluginProxyDef)(def)
+	C.free(unsafe.Pointer(p.name))
+	C.free(unsafe.Pointer(p.description))
+}
+
+func FLBPluginConfigKey(plugin unsafe.Pointer, key string) string {
+	_key := C.CString(key)
+	value := C.GoString(C.input_get_property(_key, plugin))
+	C.free(unsafe.Pointer(_key))
+	return value
+}
